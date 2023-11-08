26baf5cd269320a48f9f2169fd0178868c84e408
lsprgabriel <ogabrielpereira@pm.me>
2023-11-08 14:30:17 -0300

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Gerar Gráfico
+      </button>
+    </main>
+  )
+}
