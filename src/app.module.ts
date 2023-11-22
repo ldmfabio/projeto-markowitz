6ce6db69e5cf8dd7c98fb3585f35214a92729811
lsprgabriel <ogabrielpereira@pm.me>
2023-11-22 16:17:29 -0300

    Post module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, PostModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
