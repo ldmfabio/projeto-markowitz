edf5bcb87c447adc2c9c5c85fe5250f70038c90b
pgabriel <pgabriel@softexpert.com>
2024-01-12 15:21:26 -0300

    Adicionando python ao projeto.

---
+++ app.py
@@ -0,0 +1,172 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly
+import plotly.io as io
+from datetime import datetime
+import cvxopt as opt
+from get_selic import get_selic
+import os
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+np.random.seed(777)
+
+stocks = ["PETR4.SA", "VALE3.SA", "ITUB4.SA", "EMBR3.SA"]
+
+start_date = "2022-01-01"
+end_date = "2023-01-01"
+
+table = yf.download(stocks, start=start_date, end=end_date)['Adj Close']
+table.columns=['PETRO', 'VALE', 'ITAÚ', 'EMBRAER']
+
+# ===========================================
+
+returns = table.pct_change().dropna()
+mean_returns = returns.mean()
+cov_matrix = returns.cov()
+risk_free_rate = get_selic()
+
+# ===========================================
+
+pbar = opt.matrix(mean_returns.values)
+n = len(mean_returns)
+P = opt.matrix(cov_matrix.values)
+q = opt.matrix(0.0, (n,1))
+G = opt.matrix(0.0, (n+1,n))
+for i in range(n):
+    G[i,i] = -1.0
+G[n,:] = -pbar.T
+h = opt.matrix(0.0, (n+1,1))
+A = opt.matrix(1.0, (1,n))
+b = opt.matrix(1.0)
+
+# Construção da fronteira eficiente
+portfolios = []
+for mu in np.linspace(min(pbar), max(pbar)):
+    h[n,0] = -mu
+    portfolios.append(opt.solvers.qp(P, q, G, h, A, b)['x'])
+returns = [opt.blas.dot(pbar, x)*252 for x in portfolios]
+risks = [np.sqrt(opt.blas.dot(x, P*x)*252) for x in portfolios]
+
+os.system('cls||clear')
+
+# Portfólio de menor risco
+i_min = np.argmin(risks)
+portfolio_min_risk = risks[i_min]
+portfolio_min_return = returns[i_min]
+print("===========================================")
+print('Portfólio de Menor Risco:') # x * (10 ^ (-01))
+decimal_number = portfolios[i_min]
+percentage_number = [x * 100 for x in decimal_number]
+for i in range(len(stocks)):
+    print(f'{stocks[i]}: {percentage_number[i]:.2f}%')
+print("===========================================")
+
+# Portfólio de melhor relação risco/retorno (Índice de Sharpe)
+sharpe = [(returns[i] - risk_free_rate)/risks[i] for i in range(len(risks))]
+portfolio_sharpe = portfolios[np.argmax(sharpe)]
+
+print('Portfólio de Melhor Relação Risco/Retorno(Sharpe):')
+decimal_number = portfolio_sharpe
+percentage_number = [x * 100 for x in decimal_number]
+for i in range(len(stocks)):
+    print(f'{stocks[i]}: {percentage_number[i]:.2f}%')
+print("===========================================")
+
+# Cálculo da reta tangente
+x = np.linspace(0.0, max(risks))
+y = risk_free_rate + sharpe[np.argmax(sharpe)]*x
+
+# Cálculo do retorno e do risco de melhor relação
+returno_sharpe = opt.blas.dot(pbar, portfolio_sharpe)*252
+risk_sharpe = np.sqrt(opt.blas.dot(portfolio_sharpe, P*portfolio_sharpe)*252)
+
+# Cálculo do ponto com risco definido
+risco_requerido = 0.2
+if risco_requerido > risk_sharpe:
+    print('Risco maior que aquele associado à carteira de Sharpe ótimo!')
+    print('Não há carteira ótima com esse risco!')
+else:
+    peso_requerido = risco_requerido/risk_sharpe
+    print('Portfólio com risco definido:')
+    decimal_number = peso_requerido*portfolio_sharpe
+    percentage_number = [x * 100 for x in decimal_number]
+    for i in range(len(stocks)):
+        print(f'{stocks[i]}: {percentage_number[i][0]:.2f}%')
+    print("===========================================")
+    print('Peso no ativo livre de risco')
+    print(1.0-peso_requerido)
+    x0 = peso_requerido*risk_sharpe
+    y0 = peso_requerido*returno_sharpe + (1.0-peso_requerido)*risk_free_rate
+
+# Construção do gráfico interativo
+fig = go.Figure()
+
+# Scatter plot para a fronteira eficiente
+fig.add_trace(go.Scatter(
+    x=risks,
+    y=returns,
+    mode='lines',
+    name='Fronteira Eficiente',
+))
+
+# Ponto de menor risco
+fig.add_trace(go.Scatter(
+    x=[portfolio_min_risk],
+    y=[portfolio_min_return],
+    mode='markers',
+    marker=dict(size=10, color='red'),
+    name='Portfólio de Menor Risco',
+))
+
+# Reta tangente
+fig.add_trace(go.Scatter(
+    x=x,
+    y=y,
+    mode='lines',
+    name='Reta Tangente',
+))
+
+# Ponto da melhor relação risco/retorno (Sharpe)
+fig.add_trace(go.Scatter(
+    x=[risk_sharpe],
+    y=[returno_sharpe],
+    mode='markers',
+    marker=dict(size=10, color='green'),
+    name='Portfólio de Melhor Relação Risco/Retorno (Sharpe)',
+))
+
+# Ponto com risco definido
+fig.add_trace(go.Scatter(
+    x=[x0],
+    y=[y0],
+    mode='markers',
+    marker=dict(size=10, color='blue'),
+    name='Portfólio com Risco Definido',
+))
+
+# Atualizando o layout do gráfico
+fig.update_layout(
+    title='Fronteira Eficiente e Portfólios',
+    xaxis=dict(title='Risco'),
+    yaxis=dict(title='Retorno'),
+    showlegend=True,
+)
+
+plot = plotly.offline.plot(fig, include_plotlyjs=False, filename='file.hmtl', auto_open=True, output_type='div')
+
+data = fig.to_json()
+data = json.loads(data)
+
+@app.route('/')
+@cross_origin()
+def index():
+    return jsonify(data)
--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,26 @@ import Plot from 'react-plotly.js'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState({})
+  const [data, setData]:any = useState({})
 
-  // fetch data from http://127.0.0.1:5000/
-  const getData = async () => {
-    const response = await fetch('http://localhost:5000/').then(res => res.json())
-    setData(response)
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:5000/')
+    const data = await response.json()
+    setData(data)
   }
 
   useEffect(() => {
-    getData()
-  });
+    fetchData()
+  }, [])
 
   console.log(data)
-
   return (
     <>
       <div>
         <Plot
-          data={[
-            {
-              x: [1, 2, 3],
-              y: [2, 6, 3],
-              type: 'scatter',
-              mode: 'lines+markers',
-              marker: { color: 'red' },
-            },
-            { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
-          ]}
-          layout={{ width: 920, height: 540, title: 'A Fancy Plot' }} />
+          data={data['data']}
+          layout={data['layout']} />
+          
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
