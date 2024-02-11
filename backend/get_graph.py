a5be0bac41e00a3c5afc4c7e59f3ec244be3aa1f
lsprgabriel <ogabrielpereira@pm.me>
2024-02-11 18:05:43 -0300

    FEAT Portfolio Data: Adding each portfolio to UI

--- backend/get_graph.py
+++ backend/get_graph.py
@@ -14,13 +14,12 @@ import json
 def get_graph(start_date, end_date): 
     np.random.seed(777)
 
-    stocks = ["PETR4.SA", "VALE3.SA", "ITUB4.SA", "EMBR3.SA"]
+    stocks = ["PETR4.SA", "VALE3.SA", "ITUB4.SA", "EMBR3.SA", "BBDC4.SA"]
 
     start_date = start_date
     end_date = end_date
 
     table = yf.download(stocks, start=start_date, end=end_date)['Adj Close']
-    table.columns=['PETRO', 'VALE', 'ITAÚ', 'EMBRAER']
 
     # ===========================================
 
@@ -61,8 +60,10 @@ def get_graph(start_date, end_date):
     print('Portfólio de Menor Risco:') # x * (10 ^ (-01))
     decimal_number = portfolios[i_min]
     percentage_number = [x * 100 for x in decimal_number]
+    low_risk_portfolio = []
     for i in range(len(stocks)):
         print(f'{stocks[i]}: {percentage_number[i]:.2f}%')
+        low_risk_portfolio.append(f'{stocks[i]}: {percentage_number[i]:.2f}%')
     print("===========================================")
 
     # Portfólio de melhor relação risco/retorno (Índice de Sharpe)
@@ -72,8 +73,10 @@ def get_graph(start_date, end_date):
     print('Portfólio de Melhor Relação Risco/Retorno(Sharpe):')
     decimal_number = portfolio_sharpe
     percentage_number = [x * 100 for x in decimal_number]
+    better_risk_return_portfolio = []
     for i in range(len(stocks)):
         print(f'{stocks[i]}: {percentage_number[i]:.2f}%')
+        better_risk_return_portfolio.append(f'{stocks[i]}: {percentage_number[i]:.2f}%')
     print("===========================================")
 
     # Cálculo da reta tangente
@@ -92,14 +95,17 @@ def get_graph(start_date, end_date):
         return "bla"
     else:
         peso_requerido = risco_requerido/risk_sharpe
-        print('Portfólio com risco definido:')
+        print('Portfólio com Risco Definido:')
         decimal_number = peso_requerido*portfolio_sharpe
         percentage_number = [x * 100 for x in decimal_number]
+        defined_risk_portfolio = []
         for i in range(len(stocks)):
             print(f'{stocks[i]}: {percentage_number[i][0]:.2f}%')
+            defined_risk_portfolio.append(f'{stocks[i]}: {percentage_number[i][0]:.2f}%')
         print("===========================================")
-        print('Peso no ativo livre de risco')
-        print(1.0-peso_requerido)
+        print('Peso no Ativo Livre de Risco')
+        risk_free_rate_asset = f'Ativo Livre de Risco: {(1.0-peso_requerido)*100:.2f}%'
+        print(f'{(1.0-peso_requerido)*100:.2f}%')
         x0 = peso_requerido*risk_sharpe
         y0 = peso_requerido*returno_sharpe + (1.0-peso_requerido)*risk_free_rate
 
@@ -159,4 +165,4 @@ def get_graph(start_date, end_date):
 
     data = fig.to_json()
     data = json.loads(data)
-    return data
+    return [data, low_risk_portfolio, better_risk_return_portfolio, defined_risk_portfolio, risk_free_rate_asset]
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Plot from 'react-plotly.js'
 import { useForm } from 'react-hook-form'
 import './App.css'
@@ -7,10 +7,16 @@ function App() {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data:any) => fetchData(data);
   const [data, setData]:any = useState({})
+  const [lowRiskPortifolio, setlowRiskPortifolio ] = useState([])
+  const [betterRiskReturnPortifolio, setbetterRiskReturnPortifolio ] = useState([])
+  const [ definedRiskPortifolio, setDefinedRiskPortifolio ] = useState([])
+  const [ riskFreeAsset, setRiskFreeAsset ] = useState([])
   const baseUrl = 'http://localhost:5000'
+  const graphData = data['data']
+  const graphLayout = data['layout']
 
   const fetchData = async (data: any) => {
-    const postResponse = await fetch(`${baseUrl}/graph`, {
+    const postResponse: any = await fetch(`${baseUrl}/graph`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -18,50 +24,90 @@ function App() {
       body: JSON.stringify(data)
     })
     const postData = await postResponse.json()
-    setData(postData)
+    setData(postData[0])
+    setlowRiskPortifolio(postData[1])
+    setbetterRiskReturnPortifolio(postData[2])
+    setDefinedRiskPortifolio(postData[3])
+    setRiskFreeAsset(postData[4])
   }
 
   return (
     <>
       <h1 className="text-3xl font-bold mt-4">
-        Projeto-Markowitz
+        MKZ Frontier
       </h1>
-      <div className='flex'>
-        <div className="bg-white m-6 py-2">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className='flex items-start m-3'>
-              <div className='m-1'>
-                <label className='text-black' htmlFor="start">Data inicial:</label>
-                <input defaultValue={'2022-01-01'} className='rounded bg-gray-600' type="date" {...register("start")} />
-              </div>
-              <div className='m-1'>
-                <label className='text-black' htmlFor="start">Data final:</label>
-                <input defaultValue={'2023-01-01'} className='rounded bg-gray-600' type="date" {...register("end")} />
-              </div>
-            </div>
-            <input className='rounded bg-gray-600 p-3' type="submit" value="Submit" />
-          </form>
-        </div>
-        <div className='m-6'>
-          <Plot
-            data={data['data']}
-            layout={{ 
-                title: "Fronteira Eficiente",
-                titlefont: {
-                  size: 20,
-                },
-                width: 940, 
-                height: 580, 
-                plot_bgcolor: '#E5ECF6',
-                yaxis: {
-                  title: "Retorno",
-                },
-                xaxis: {
-                  title: "Risco",
-                }
+      <div className='flex justify-center'>
+        <div>
+          <div className='m-6'>
+            <Plot
+              data={graphData}
+              layout={{
+                ...graphLayout,
+                width: 1000,
+                height: 600,
+              
               }}
-          />
+            />
+          </div>
+
+          <div className="bg-white m-6 py-2">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className='flex justify-start m-5'>
+                <div className='m-1'>
+                  <label className='text-black'>Data inicial:</label>
+                  <input defaultValue={'2022-01-01'} className='rounded bg-gray-600' type="date" {...register("start")} />
+                </div>
+                
+                <div className='m-1'>
+                  <label className='text-black'>Data final:</label>
+                  <input defaultValue={'2023-01-01'} className='rounded bg-gray-600' type="date" {...register("end")} />
+                </div>
+              </div>
+              <input className='rounded bg-gray-600 p-3' type="submit" value="Submit" />
+            </form>
+          </div>
         </div>
+
+        <div className="bg-white">
+          <div className='text-justify'>
+            <div className='m-5'>
+              <h1 className='text-black font-bold'>Portfólio de Menor Risco:</h1>
+              <ul>
+                {lowRiskPortifolio.map((stock: any) => (
+                  <li className='text-black' key={stock}>
+                    {stock}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className='m-5'>
+              <h1 className='text-black font-bold'>Portfólio de Melhor Relação Risco/Retorno:</h1>
+              <ul>
+                {betterRiskReturnPortifolio.map((stock: any) => (
+                  <li className='text-black' key={stock}>
+                    {stock}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className='m-5'>
+              <h1 className='text-black font-bold'>Portifólio com Risco Definido:</h1>
+              <ul>
+                {definedRiskPortifolio.map((stock: any) => (
+                  <li className='text-black' key={stock}>
+                    {stock}
+                  </li>
+                ))}
+                <li className='text-black'>
+                  {riskFreeAsset}
+                </li>
+              </ul>
+            </div>
+          </div>
+          
+          
+        </div>
+
       </div>
     </>
   )
