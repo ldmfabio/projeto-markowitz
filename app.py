00bd69f4324e7941a96b2cb1c35ef266b86f0b4a
mateus-lopes <mateusalbano22@gmail.com>
2024-04-12 14:44:17 -0300

    FEAT: Testing the streamlit authenticator

--- app.py
+++ app.py
@@ -1,48 +1,68 @@
 import streamlit as st
-
 import streamlit_authenticator as stauth
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 import yaml
 from yaml.loader import SafeLoader
 
-
-
 def main():
-    st.set_page_config(
-        page_title="Projeto Markowitz", 
-        page_icon="💣", 
-        layout="wide", 
-        initial_sidebar_state="expanded", 
-        menu_items={
-            'Get Help': 'https://www.extremelycoolapp.com/help',
-            'Report a bug': "https://www.extremelycoolapp.com/bug",
-            'About': """About this app: This is a cool app that does cool things. Have fun! 🚀"""
-        }
-    )  
-
-    with st.sidebar:
-        pass
-
     with open('./config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
-    
-    # authenticator = Authenticate(
-    #     config['credentials'],
-    #     config['cookie']['name'],
-    #     config['cookie']['key'],
-    #     config['cookie']['expiry_days'],
-    #     config['preauthorized']
-    # )
 
-    # name, authentication_status, username = authenticator.login('Login', 'main')
-    # st.write(name, authentication_status, username)
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
 
-    st.write('Hello, world!')
+    authenticator.login()
 
-if __name__ == '__main__':
+    if st.session_state["authentication_status"]:
+        try:
+            if authenticator.reset_password(st.session_state["username"]):
+                with open('./config.yaml', 'w') as file:
+                    yaml.dump(config, file, default_flow_style=False)
+                st.success('Password modified successfully')
+        except Exception as e:
+            st.error(e)
+
+    try:
+        email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+        if email_of_registered_user:
+            with open('./config.yaml', 'w') as file:
+                yaml.dump(config, file, default_flow_style=False)
+            st.success('User registered successfully')
+    except Exception as e:
+        st.error(e)
+
+    try:
+        username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+        if username_of_forgotten_password:
+            st.success('New password to be sent securely')
+            # The developer should securely transfer the new password to the user.
+        elif username_of_forgotten_password == False:
+            st.error('Username not found')
+    except Exception as e:
+        st.error(e)
+
+    try:
+        username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
+        if username_of_forgotten_username:
+            st.success('Username to be sent securely')
+            # The developer should securely transfer the username to the user.
+        elif username_of_forgotten_username == False:
+            st.error('Email not found')
+    except Exception as e:
+        st.error(e)
+
+    if st.session_state["authentication_status"]:
+        try:
+            if authenticator.update_user_details(st.session_state["username"]):
+                with open('./config.yaml', 'w') as file:
+                    yaml.dump(config, file, default_flow_style=False)
+                st.success('Entries updated successfully')
+        except Exception as e:
+            st.error(e)
+                
+if __name__ == "__main__":
     main()
